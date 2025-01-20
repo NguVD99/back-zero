e4152c9a3c302bd87b010359c63e853aa11b77e0
NguVD99 <vungu10031999@gmail.com>
2025-01-20 16:53:43 +0800

    updet ket noi database

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express'); //commonjs
 const configViewEngine = require('./config/viewEngine');
 const webRouter = require('./routes/web');
+const mysql = require('mysql2');
 
 
 // console.log(">>> check env:", process.env);
@@ -14,6 +15,25 @@ configViewEngine(app);
 
 // Khai bao route
 app.use('/', webRouter);
+
+// Test connection
+// Create the connection to database
+const connection = mysql.createConnection({
+    host: '89.233.105.193',
+    port: 3306,
+    user: 'db_ngu_dev',
+    password: '123123123Aa',
+    database: 'db_ngu_dev',
+});
+
+// A simple SELECT query
+connection.query(
+    'SELECT * FROM Users u',
+    function (err, results, fields) {
+        console.log('>>>Results', results); // results contains rows returned by server
+        console.log('>>>Fields', fields); // fields contains extra meta data about results, if available
+    }
+);
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
