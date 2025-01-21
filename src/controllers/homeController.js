46e1408d4f5fa578ffdf2e9ed17a2b75a74f43cb
NguVD99 <vungu10031999@gmail.com>
2025-01-21 18:08:39 +0800

    32

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,25 @@
+const connection = require('../config/database');
+
+
 
 const getHomepage = (req, res) => {
-    res.send('Hello World! test')
+    //Process data
+    //call model
+    let users = [];
+
+    connection.query(
+        'SELECT * FROM Users u',
+        function (err, results, fields) {
+            users = results;
+            console.log('>>>Results', results); // results contains rows returned by server
+            console.log('>>>Fields', fields); // fields contains extra meta data about results, if available
+
+            console.log(">> check users: ", users)
+            res.send(JSON.stringify(users))
+        }
+    );
+
+
 }
 
 const getABC = (req, res) => {
